15d4e13c495510a69a1f64c26285cea1ab497f66
Yash Hegde <yash4success@gmail.com>
2024-02-10 17:52:33 +0530

    typo

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,6 +1,6 @@
 const mongoose  = require("mongoose");
 
-const postSchema = new mongoose.schema({
+const postSchema = new mongoose.Schema({
     title: String,
     image: {
         public_id: String,
